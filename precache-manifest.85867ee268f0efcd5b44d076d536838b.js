5365d97fc66dc788bd7444ff27db576696868ded
Shanelie Fernandes <shanelieqp@yahoo.com>
2020-06-09 00:01:21 +0100

    Updates

---
+++ precache-manifest.85867ee268f0efcd5b44d076d536838b.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41b92e068609c4507b3f88a1b24b4381",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d9b5a7915664dd5c2316",
+    "url": "/Portfolio/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "8c5f0554f54b721e93f4",
+    "url": "/Portfolio/static/css/main.5faf4286.chunk.css"
+  },
+  {
+    "revision": "d9b5a7915664dd5c2316",
+    "url": "/Portfolio/static/js/2.cb491f91.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio/static/js/2.cb491f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5f0554f54b721e93f4",
+    "url": "/Portfolio/static/js/main.df26df7e.chunk.js"
+  },
+  {
+    "revision": "fb573d3954dc3a670a45",
+    "url": "/Portfolio/static/js/runtime-main.e86b5c16.js"
+  },
+  {
+    "revision": "8cb380bf02a4d62e63f1eff27175b41f",
+    "url": "/Portfolio/static/media/SE.8cb380bf.png"
+  },
+  {
+    "revision": "a511aea672a2cd08596ed691734b5201",
+    "url": "/Portfolio/static/media/f1.a511aea6.png"
+  },
+  {
+    "revision": "d9b8140d983d3a83316858f05442951d",
+    "url": "/Portfolio/static/media/f2.d9b8140d.png"
+  },
+  {
+    "revision": "1b0acbb90836537b319a32268dfff7c7",
+    "url": "/Portfolio/static/media/f3.1b0acbb9.png"
+  },
+  {
+    "revision": "726de5beae7c7d0e695f39ed916388f8",
+    "url": "/Portfolio/static/media/f4.726de5be.png"
+  },
+  {
+    "revision": "21045d9c676f4ecf834df3ee24a114c3",
+    "url": "/Portfolio/static/media/flowchart.21045d9c.png"
+  },
+  {
+    "revision": "6010cb69e157eb6e130660b8acfede74",
+    "url": "/Portfolio/static/media/graph.6010cb69.png"
+  },
+  {
+    "revision": "20a3a9d9b8b95d756b473843355d9bcf",
+    "url": "/Portfolio/static/media/img1.20a3a9d9.jpg"
+  },
+  {
+    "revision": "b5959b6c5f7901fccccb120b75b0512b",
+    "url": "/Portfolio/static/media/img2.b5959b6c.jpg"
+  },
+  {
+    "revision": "90aea32f35873531296316e3ec643ebd",
+    "url": "/Portfolio/static/media/img3.90aea32f.jpg"
+  },
+  {
+    "revision": "52427c5a0fdd62f68e502abbb26adf8c",
+    "url": "/Portfolio/static/media/img4.52427c5a.jpg"
+  },
+  {
+    "revision": "ac2da38af4fd536594f697a212244cd5",
+    "url": "/Portfolio/static/media/img5.ac2da38a.jpg"
+  },
+  {
+    "revision": "0fafcdcc14d7bb0545ddf05ad1ab5b4f",
+    "url": "/Portfolio/static/media/img6.0fafcdcc.jpg"
+  },
+  {
+    "revision": "12c1d1e21899152da22061d4432dc63d",
+    "url": "/Portfolio/static/media/img7.12c1d1e2.jpg"
+  },
+  {
+    "revision": "6742500a050ceb58f8b5d4c1c1b1ec83",
+    "url": "/Portfolio/static/media/img8.6742500a.jpg"
+  },
+  {
+    "revision": "deaa79dc4f6fe94fbb1a6bce9b828def",
+    "url": "/Portfolio/static/media/map1.deaa79dc.png"
+  },
+  {
+    "revision": "fd907d5c329c395afe572d3cc1689309",
+    "url": "/Portfolio/static/media/map2.fd907d5c.png"
+  },
+  {
+    "revision": "f7b15f88e14c9f2c720d10a2d27fe51e",
+    "url": "/Portfolio/static/media/md.f7b15f88.jpeg"
+  },
+  {
+    "revision": "61ec34460fb5735f1fb46a9cf9f5c276",
+    "url": "/Portfolio/static/media/mv.61ec3446.png"
+  },
+  {
+    "revision": "14d86edc9ff2381e7b072006678dd3e3",
+    "url": "/Portfolio/static/media/p6.14d86edc.png"
+  },
+  {
+    "revision": "57457bf49adb61eed9fbd692149ee990",
+    "url": "/Portfolio/static/media/pages.57457bf4.png"
+  },
+  {
+    "revision": "13988ffaf07386d61d89a9822041c513",
+    "url": "/Portfolio/static/media/space.13988ffa.png"
+  },
+  {
+    "revision": "a329d7595f63b2e332663a77e3c00d0a",
+    "url": "/Portfolio/static/media/viruscells.a329d759.png"
+  },
+  {
+    "revision": "fbeab31a447bcdfb359cd1f716f44d25",
+    "url": "/Portfolio/static/media/vt.fbeab31a.png"
+  }
+]);
